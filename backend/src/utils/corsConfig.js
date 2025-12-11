19bb0a022fd4a87268653f98cc3225d5307b7af2
eyueal191 <eyuealayalew74@gmail.com>
2025-12-11 01:39:46 -0800

    change to static cors origin

--- backend/src/utils/corsConfig.js
+++ backend/src/utils/corsConfig.js
@@ -1,11 +1,12 @@
 // src/utils/corsConfig.js
 export function dynamicCorsOptions(allowedMethods = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]) {
   return {
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true); // allow Postman / server-to-server
-      console.log("🌍 CORS request from:", origin);
-      callback(null, true); // allow all origins dynamically
-    },
+    // (origin, callback) => {
+    //   if (!origin) return callback(null, true); // allow Postman / server-to-server
+    //   console.log("🌍 CORS request from:", origin);
+    //   callback(null, true); // allow all origins dynamically
+    // }
+    origin: ["http://localhost:4173","http://localhost:5173"],
     credentials: true, // allows cookies / auth headers
     methods: allowedMethods, // use the passed methods
     allowedHeaders: ["Content-Type", "Authorization"],
